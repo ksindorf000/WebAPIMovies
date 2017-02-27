adbc265cc08524859f9c913fe3381ae1e0a952c3
ksindorf000 <ksindorf000@gmail.com>
2017-02-27 13:34:03 -0500

    Initial

---
+++ SpoiledApplesTheApi/Migrations/201702271833174_Initial.cs
@@ -0,0 +1,31 @@
+namespace SpoiledApplesTheApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Genre = c.String(),
+                        IMDB = c.String(),
+                        imgURL = c.String(),
+                        ReleaseDate = c.String(),
+                        AverageRating = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
---
+++ SpoiledApplesTheApi/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpoiledApplesTheApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string IMDB { get; set; }
+        public string imgURL { get; set; }
+        public string ReleaseDate { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
